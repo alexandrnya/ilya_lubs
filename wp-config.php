0bde6ab3541a77ddddb9ffd3e0fce37a97de270c
Zhuravlev-AG <zhuravlev-ag@biryusa.ru>
2019-11-29 13:15:24 +0700

    config

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'lab6' );
 define( 'DB_USER', 'root' );
 
 /** Пароль к базе данных MySQL */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'secret' );
 
 /** Имя сервера MySQL */
 define( 'DB_HOST', 'localhost' );
