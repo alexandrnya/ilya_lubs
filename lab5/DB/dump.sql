a2935938fcbd59b6a8d1b8745ad19b9ee8abab39
Александр <alexandrnya1997@mail.ru>
2019-12-15 18:12:35 +0700

    db

--- lab5/DB/dump.sql
+++ lab5/DB/dump.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.43, for Win32 (AMD64)
+-- MySQL dump 10.13  Distrib 5.6.41, for Win32 (AMD64)
 --
--- Host: localhost    Database: lab4
+-- Host: localhost    Database: lab5
 -- ------------------------------------------------------
--- Server version	5.6.43
+-- Server version	5.6.41
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,13 +27,10 @@ CREATE TABLE `comments` (
   `users_id` int(10) DEFAULT NULL,
   `comments_comment` text NOT NULL,
   `comments_created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `news_id` int(10) NOT NULL,
   PRIMARY KEY (`comments_id`),
   KEY `users_id` (`users_id`),
-  KEY `fk_comments_news1` (`news_id`),
-  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`users_id`) ON DELETE SET NULL,
-  CONSTRAINT `fk_comments_news1` FOREIGN KEY (`news_id`) REFERENCES `news` (`news_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`users_id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +39,6 @@ CREATE TABLE `comments` (
 
 LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` VALUES (11,3,'123','2019-11-15 03:55:04',8),(13,3,'Всем привет','2019-11-15 04:10:48',8);
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,7 +61,7 @@ CREATE TABLE `news` (
   PRIMARY KEY (`news_id`),
   KEY `users_id` (`users_id`),
   CONSTRAINT `news_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`users_id`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,8 +70,143 @@ CREATE TABLE `news` (
 
 LOCK TABLES `news` WRITE;
 /*!40000 ALTER TABLE `news` DISABLE KEYS */;
-INSERT INTO `news` VALUES (8,NULL,'','','','ХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текст','2019-11-10 10:54:25',''),(10,NULL,'/lab4/uploads/news/detail/bez_nazvaniya.png','/lab4/uploads/news/view/','<h2>Привет</h2>\r\nРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст ','','2019-11-10 12:11:15','Вторая новость');
+INSERT INTO `news` VALUES (8,2,'','','','ХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текстХА рыбныйы текст','2019-11-10 10:54:25',''),(10,2,'/lab4/uploads/news/detail/bez_nazvaniya.png','/lab4/uploads/news/view/','<h2>Привет</h2>\r\nРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текстРыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст Рыбный текст ','','2019-11-10 12:11:15','Вторая новость'),(11,3,'/lab4/uploads/news/detail/depositphotos_96432414-stock-illustration-user-icon-support-vectors-design.jpg','/lab4/uploads/news/view/depositphotos_100074264-stock-illustration-lettering-mom-and-mom-in.jpg','yiuyi','rtyrty','2019-11-16 08:22:24','первая новость');
 /*!40000 ALTER TABLE `news` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(100) DEFAULT NULL,
+  `PRICE` decimal(19,4) DEFAULT NULL,
+  `SECTION_ID` int(11) NOT NULL,
+  `ACTIVE` int(1) DEFAULT '1',
+  `PICTURE` text,
+  PRIMARY KEY (`ID`),
+  KEY `fk_product_section1_idx` (`SECTION_ID`),
+  CONSTRAINT `fk_product_section1` FOREIGN KEY (`SECTION_ID`) REFERENCES `section` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Gigabyte GeForce GTX 1660 Ti GAMING OC [GV-N166TGAMING OC-6GD]',21599.0000,3,1,'/lab5/uploads/Gigabyte GeForce GTX 1660 Ti GAMING OC [GV-N166TGAMING OC-6GD].jpg');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_property`
+--
+
+DROP TABLE IF EXISTS `product_property`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_property` (
+  `PRODUCT_ID` int(11) NOT NULL,
+  `PROPERTY_ID` int(11) NOT NULL,
+  PRIMARY KEY (`PRODUCT_ID`,`PROPERTY_ID`),
+  KEY `fk_product_property_product1_idx` (`PRODUCT_ID`),
+  KEY `fk_product_property_property1_idx` (`PROPERTY_ID`),
+  CONSTRAINT `fk_product_property_product1` FOREIGN KEY (`PRODUCT_ID`) REFERENCES `product` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_product_property_property1` FOREIGN KEY (`PROPERTY_ID`) REFERENCES `property` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_property`
+--
+
+LOCK TABLES `product_property` WRITE;
+/*!40000 ALTER TABLE `product_property` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_property` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `property`
+--
+
+DROP TABLE IF EXISTS `property`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `property` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `property`
+--
+
+LOCK TABLES `property` WRITE;
+/*!40000 ALTER TABLE `property` DISABLE KEYS */;
+/*!40000 ALTER TABLE `property` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `property_value`
+--
+
+DROP TABLE IF EXISTS `property_value`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `property_value` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `PROPERTY_ID` int(11) NOT NULL,
+  `VALUE` text,
+  PRIMARY KEY (`ID`),
+  KEY `fk_property_value_property1_idx` (`PROPERTY_ID`),
+  CONSTRAINT `fk_property_value_property1` FOREIGN KEY (`PROPERTY_ID`) REFERENCES `property` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `property_value`
+--
+
+LOCK TABLES `property_value` WRITE;
+/*!40000 ALTER TABLE `property_value` DISABLE KEYS */;
+/*!40000 ALTER TABLE `property_value` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `section`
+--
+
+DROP TABLE IF EXISTS `section`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `section` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(100) DEFAULT NULL,
+  `PICTURE` text,
+  `ACTIVE` int(1) DEFAULT '1',
+  `PARENT_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `parent_section_id_idx` (`PARENT_ID`),
+  CONSTRAINT `parent_section_id` FOREIGN KEY (`PARENT_ID`) REFERENCES `section` (`ID`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `section`
+--
+
+LOCK TABLES `section` WRITE;
+/*!40000 ALTER TABLE `section` DISABLE KEYS */;
+INSERT INTO `section` VALUES (3,'Видеокарты',NULL,1,NULL);
+/*!40000 ALTER TABLE `section` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -120,9 +251,9 @@ CREATE TABLE `users` (
   `users_created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `themes_id` int(10) NOT NULL DEFAULT '1',
   PRIMARY KEY (`users_id`),
-  KEY `fk_users_themes1` (`themes_id`),
+  KEY `fk_users_themes1_idx1` (`themes_id`),
   CONSTRAINT `fk_users_themes1` FOREIGN KEY (`themes_id`) REFERENCES `themes` (`themes_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,7 +262,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (3,'Zhuravlev-AG','zhuravlev-ag@biryusa.ru','d46088c1f4648e16a40573c1a5cb6658','a','b','2019-11-15 02:58:15',2),(4,'Zhuravlev-AG','zhuravlev-ag@biryusa.ru','d46088c1f4648e16a40573c1a5cb6658','a','b','2019-11-15 04:46:28',2);
+INSERT INTO `users` VALUES (1,'alexandrnya','a@mail.ru','e10adc3949ba59abbe56e057f20f883e','A','b','2019-11-05 12:06:57',1),(2,'alexandrnya','a@mail.ru','e10adc3949ba59abbe56e057f20f883e','A','b','2019-11-10 10:43:03',1),(3,'Zhuravlev-AG','alexandrnya1997@gmail.com','e10adc3949ba59abbe56e057f20f883e','A','Zhuravliov','2019-11-16 08:21:38',1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -144,4 +275,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-15 13:24:51
+-- Dump completed on 2019-12-15 18:11:38
