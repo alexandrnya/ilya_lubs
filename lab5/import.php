2529b9504bd0da97764fc7eda655e308cb3af2b4
Zhuravlev-AG <zhuravlev-ag@biryusa.ru>
2019-12-18 09:36:03 +0700

    Импорт товаров работает!! Файл должен лежать в "/lab5/DB/catalog.csv". Позваляет добавлять и обновлять товар Разделитель по умолчанию точка с запятой. Первая строка содержит следующие заголовки: NAME - название товара (предполагается, что нет товаров с одинаковым называнием) PRICE - цена SECTION_NAME - название категории (предполагается, что нет категорий с одинаковым называнием) PICTURE - путь к картинке (например /lab5/uploads/Gigabyte GeForce GTX 1660 Ti GAMING OC [GV-N166TGAMING OC-6GD].jpg) Следующие колонки - это названия свойств товара Множественные значения указываются через запятую

--- lab5/import.php
+++ lab5/import.php
@@ -1,56 +1,86 @@
 <?php
 include "Classes/autoload.php";
+
 $csv = file_get_contents($_SERVER["DOCUMENT_ROOT"]."/lab5/DB/catalog.csv");
 $csv = iconv('Windows-1251', 'UTF-8', $csv);
-$delimiter = ",";
+$delimiter = ";";
+
 if($csv) {
     $arProducts = [];
     $rows = explode("\n", $csv);
-    $arHeaders = explode($delimiter, $rows[0]);
+    $arHeaders = explode($delimiter, trim($rows[0]));
+    
     unset($rows[0]);
     foreach($rows as $row) {
-        $cols = explode($delimiter, $row);
-        foreach($arHeaders as $keyHeader => $nameHeader) {
-            $id = $cols[array_search("ID", $arHeaders)];
-            if($id) {
+        $arProduct = [];
+        $cols = explode($delimiter, trim($row));
+        if($cols[array_search("NAME", $arHeaders)]) {
+            foreach($arHeaders as $keyHeader => $nameHeader) {            
                 switch($nameHeader) {
                     case "NAME":
                     case "PRICE":
-                    case "ID" :
                     case "SECTION_NAME" :
                     case "PICTURE" :
                     {
-                        $arProducts[$id][$nameHeader] =
+                        $arProduct[$nameHeader] =
+                            // Удалим пробелы и ковычки если они есть
                             preg_replace(["|^\"|", "|\"$|", "|^ |", "| $|"], "", $cols[$keyHeader]);
                         break;
                     }
                     default:
                     {
-                        $arProducts[$id]["PROPERTIES"][$nameHeader][] =
+                        $arProduct["PROPERTIES"][$keyHeader] =
+                            // Удалим пробелы и ковычки если они есть
                             preg_replace(["|^\"|", "|\"$|", "|^ |", "| $|"], "", $cols[$keyHeader]);
                     }
                 }
             }
         }
+        if(!empty($arProduct)) {
+            $arProducts[] = $arProduct;
+        }
     }
+    
     foreach($arProducts as $product) {
-        $arProps = $product["PROPERTIES"];
+        foreach($product["PROPERTIES"] as $keyProp => $valueProp) {
+            $arProps[$keyProp] = explode(",", $valueProp);
+        }
         unset($product["PROPERTIES"]);
         $res = $mysqli->query("SELECT ID FROM section WHERE NAME = '$product[SECTION_NAME]'");
         if($res && $dbSecrion = $res->fetch_assoc()) {
             $idSection = $dbSecrion["ID"];
-        } else {
+        }
+        else {
             $res = $mysqli->query("INSERT INTO section(NAME) VALUE('$product[SECTION_NAME]')");
             $idSection = $mysqli->insert_id;
         }
-        
+
         $res = $mysqli->query("SELECT ID FROM product WHERE NAME = '$product[NAME]'");
         if($res && $dbProduct = $res->fetch_assoc()) {
             $idProduct = $dbProduct["ID"];
-        } else {
+            $res = $mysqli->query("UPDATE product SET PRICE = '$product[PRICE]', PICTURE = '$product[PICTURE]', SECTION_ID = $idSection WHERE ID = $idProduct");
+        }
+        else {
             $res = $mysqli->query("INSERT INTO product(NAME,PRICE,PICTURE,SECTION_ID) value('$product[NAME]','$product[PRICE]','$product[PICTURE]',$idSection)");
             $idProduct = $mysqli->insert_id;
         }
         
+        foreach($arProps as $keyProp => $arValuesProp) {
+            $nameProp = $arHeaders[$keyProp];
+            $res = $mysqli->query("SELECT ID FROM product_property WHERE NAME = '$nameProp'");
+            if($res && $dbProperty = $res->fetch_assoc()) {
+                $idProperty = $dbProperty["ID"];
+            }
+            else {
+                $res = $mysqli->query("INSERT INTO product_property(NAME) value('$nameProp')");
+                $idProperty = $mysqli->insert_id;
+            }
+            $mysqli->query("DELETE FROM product_property_value WHERE PRODUCT_ID = $idProduct AND PRODUCT_PROPERTY_ID = $idProperty");
+            foreach($arValuesProp as $valueProp) {                
+                $res = $mysqli->query("INSERT INTO product_property_value(PRODUCT_ID, PRODUCT_PROPERTY_ID, VALUE) VALUE($idProduct, $idProperty, '$valueProp')");
+                $idValue = $mysqli->insert_id;
+            }
+        }
     }
+    echo "Success!";
 }
